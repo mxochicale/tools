3b427b0d04424e763fdf7f4e034bcc233008128c
Miguel Xochicale <miguel.xochicale@kcl.ac.uk>
2022-10-21 15:29:54 +0100

    updates ubuntu basics without vim-gtk

--- ubuntu/basics.sh
+++ ubuntu/basics.sh
@@ -8,7 +8,7 @@
 sudo apt-get update
 sudo apt-get -f upgrade
 sudo apt-get install git
-sudo apt-get install vim vim-gtk
+sudo apt-get install vim
 sudo apt-get install xsel
 sudo apt-get install tree
 sudo apt install make 
