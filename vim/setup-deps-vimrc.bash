ed6dd357fc1e4de1e7c57fd4ee6c3da4a97db0a3
Miguel Xochicale <perez.xochicale@gmail.com>
2024-12-29 16:04:21 +0000

    updates bash link for vims

--- vim/setup-deps-vimrc.bash
+++ vim/setup-deps-vimrc.bash
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt install vim vim-gtk
 cd
-wget https://raw.githubusercontent.com/mxochicale/tools/master/vim/.vimrc
+wget https://raw.githubusercontent.com/mxochicale/tools/refs/heads/main/vim/setup-deps-vimrc.bash
