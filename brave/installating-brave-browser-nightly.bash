e564d6a50350cdad38af5520510071b0cfc33288
Miguel Xochicale <perez.xochicale@gmail.com>
2025-06-28 17:40:34 +0100

    brave-browser-nightly 138.1.82.31 #12

--- brave/installating-brave-browser-nightly.bash
+++ brave/installating-brave-browser-nightly.bash
@@ -11,7 +11,7 @@
 # Reference
 # https://brave.com/linux/#nightly-channel-installation
 
-sudo apt install apt-transport-https curl
+sudo apt install -y apt-transport-https curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg https://brave-browser-apt-nightly.s3.brave.com/brave-browser-nightly-archive-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg] https://brave-browser-apt-nightly.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-nightly.list
