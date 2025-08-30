101fb7e2f7b0d5fd4382cfea56f0041c99e49cb2
Miguel Xochicale <perez.xochicale@gmail.com>
2025-08-30 10:20:51 +0100

    updates correct vimrc link that use raw.githubusercontent.com

--- vim/setup-deps-vimrc.bash
+++ vim/setup-deps-vimrc.bash
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt install -y vim vim-gtk3
 cd
-wget https://github.com/mxochicale/tools/blob/main/vim/.vimrc
+wget https://raw.githubusercontent.com/mxochicale/tools/refs/heads/main/vim/.vimrc
